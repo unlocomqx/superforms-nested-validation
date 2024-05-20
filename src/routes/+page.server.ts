58f55db3a2184756cc9c74607f415463f628cfcf
unloco <amxmodx.unloco@gmail.com>
2024-05-20 19:18:00 +0100

    improve repro

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,7 +6,7 @@ const schema = z.object({
     id: z.string(),
     options: z.record(z.string(), z.object({
         label: z.string().refine(value => value.length > 0, {
-            message: "Label is required",
+            message: "Label is required"
         }),
     })),
 })
@@ -20,6 +20,14 @@ let row = {
         "2": {
             label: "",
         },
+    }
+}
+
+export const actions = {
+    async save({request}) {
+        const form = await superValidate(request, zod(schema))
+
+        console.log(form)
     }
 }
 
